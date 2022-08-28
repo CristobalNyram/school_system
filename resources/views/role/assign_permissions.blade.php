59c910b01367b375681104a3558aaa66e7802d19
cristobal nyram <crisco.nyram@gmail.com>
2022-08-28 10:42:20 -0500

    advcance

--- resources/views/role/assign_permissions.blade.php
+++ resources/views/role/assign_permissions.blade.php
@@ -22,42 +22,51 @@
                         <form class="m-5"  action="{{route('role_assign_permission')}}"  method="POST">
                             @csrf
 
-                            @foreach ($menus as  $menu)
+                            <div class="row">
+                                @foreach ($menus as  $menu)
 
-                                @if ($menu->menus->menu_parent==0)
-
-
-                                   <div class="col-lg-4 col-12">
-                                            <ul class="list-group">
-                                                <li class="list-group-item">
-                                                    <div class="custom-control custom-checkbox mb-3">
-                                                                <input class="custom-control-input " @if ($menu->status ==2) checked @endif
-                                                                id="{{ $menu->id }}" type="checkbox">
-                                                                <label class="custom-control-label " for="{{ $menu->id }}"> {{ $menu->menus->title }}</label>
-                                                </div>
-                                                    @foreach ($menus as $menus_child)
-                                                        @if ($menu->menus->id == $menus_child->menus->menu_parent)
-                                                                <li class="list-group-item ml-5">
-                                                                    <div class="custom-control custom-checkbox mb-3">
-                                                                                <input class="custom-control-input " @if ($menus_child->status ==2) checked @endif
-                                                                                id="{{ $menus_child->id }}" type="checkbox">
-                                                                                <label class="custom-control-label " for="{{ $menus_child->id }}"> {{ $menus_child->menus->title }}</label>
-                                                                    </div>
-                                                                </li>
-                                                        @endif
-                                                    @endforeach
-
-                                                </li>
-
-                                            </ul>
+                                        @if ($menu->menus->menu_parent==0)
 
 
-                                   </div>
-                                 @endif
+                                                        <div class="col-lg-4 mb-4">
+                                                                    <ul class="list-group">
+                                                                        <li class="list-group-item">
+                                                                            <div class="custom-control custom-checkbox mb-3">
+                                                                                        <input class="custom-control-input " @if ($menu->status ==2) checked @endif
+                                                                                        id="{{ $menu->id }}" type="checkbox">
+                                                                                        <label class="custom-control-label " for="{{ $menu->id }}"> {{ $menu->menus->title }}</label>
+                                                                                </div>
+
+                                                                                    @foreach ($menus as $menus_child)
+                                                                                        @if ($menu->menus->id == $menus_child->menus->menu_parent)
+                                                                                                <li class="list-group-item ml-5">
+                                                                                                    <div class="custom-control custom-checkbox mb-3">
+                                                                                                                <input class="custom-control-input " @if ($menus_child->status ==2) checked @endif
+                                                                                                                id="{{ $menus_child->id }}" type="checkbox">
+                                                                                                                <label class="custom-control-label " for="{{ $menus_child->id }}"> {{ $menus_child->menus->title }}</label>
+                                                                                                    </div>
+                                                                                                </li>
+                                                                                        @endif
+                                                                                    @endforeach
+
+                                                                        </li>
+
+                                                                </ul>
+
+
+                                                    </div>
+                                            @endif
 
 
 
-                            @endforeach
+                                  @endforeach
+
+
+
+
+
+
+                            </div>
 
 
                         </form>
