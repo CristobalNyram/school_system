5f1c7d3d4f38e8ad0b33b3f800cf277aaa04ca60
cristobal nyram <crisco.nyram@gmail.com>
2022-08-29 16:41:26 -0500

    I have included the library database

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -9,10 +9,11 @@
 <link rel="stylesheet" href="/assets/vendor/sweetalert2/dist/sweetalert2.min.css">
 
 
-<script src="/assets/vendor/datatables.net/js/jquery.dataTables.min.js"></script>
+
+<script src="/assets/vendor/datatables.net/js/jquery.dataTables.min.js" defer></script>
 
 <link rel="stylesheet" href="/assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
-<script src="/assets/vendor/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="/assets/vendor/datatables.net-bs4/js/dataTables.bootstrap4.min.js" defer></script>
 
 
 {{--  scripts end  --}}
--- resources/views/users/users/index.blade.php
+++ resources/views/users/users/index.blade.php
@@ -20,8 +20,13 @@
                 <h3 class="mb-0">Usuarios registrados</h3>
               </div>
               <!-- Light table -->
+              <script>
+      $(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+              </script>
               <div class="table-responsive">
-                <table class="table align-items-center table-flush">
+                <table class="table align-items-center table-flush" id="myTable">
                   <thead class="thead-light">
                     <tr>
                       <th scope="col" class="sort" data-sort="name">ID</th>
@@ -93,31 +98,7 @@
                 </table>
               </div>
               <!-- Card footer -->
-              <div class="card-footer py-4">
-                <nav aria-label="...">
-                  <ul class="pagination justify-content-end mb-0">
-                    <li class="page-item disabled">
-                      <a class="page-link" href="#" tabindex="-1">
-                        <i class="fas fa-angle-left"></i>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                    </li>
-                    <li class="page-item active">
-                      <a class="page-link" href="#">1</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">
-                        <i class="fas fa-angle-right"></i>
-                        <span class="sr-only">Next</span>
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
+
             </div>
           </div>
         </div>
