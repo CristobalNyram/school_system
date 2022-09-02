25999b882ca556c264ee1a4febc778cca5dc28ad
cristobal nyram <crisco.nyram@gmail.com>
2022-09-02 14:20:15 -0500

    improve in profile

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -60,26 +60,26 @@
                 </a>
                 <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
                     <div class=" dropdown-header noti-title">
-                        <h6 class="text-overflow m-0">{{ __('Welcome!') }}</h6>
+                        <h6 class="text-overflow m-0">¡Bienvenid@ {{   auth()->user()->name }} !</h6>
                     </div>
                     <a href="{{ route('profile.edit') }}" class="dropdown-item">
-                        <i class="ni ni-single-02"></i>
-                        <span>{{ __('My profile') }}</span>
+                        <i class="ni ni-single-02 text-primary"></i>
+                        <span>{{ __('Mi perfil') }}</span>
                     </a>
-                    <a href="#" class="dropdown-item">
+                    {{-- <a href="#" class="dropdown-item">
                         <i class="ni ni-settings-gear-65"></i>
-                        <span>{{ __('Settings') }}</span>
-                    </a>
+                        <span>{{ __('Configuración') }}</span>
+                    </a> --}}
 
                     <a href="#" class="dropdown-item">
-                        <i class="ni ni-support-16"></i>
-                        <span>{{ __('Support') }}</span>
+                        <i class="ni ni-support-16 text-primary"></i>
+                        <span>{{ __('Soporte') }}</span>
                     </a>
                     <div class="dropdown-divider"></div>
                     <a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
-                        <i class="ni ni-user-run"></i>
-                        <span>{{ __('Logout') }}</span>
+                        <i class="ni ni-user-run text-danger"></i>
+                        <span>{{ __('Cerrar sesión') }}</span>
                     </a>
                 </div>
             </li>
