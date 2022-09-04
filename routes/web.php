3e36a7ab2b9e887acdeede6b44f6b56d2d630a8d
cristobal nyram <crisco.nyram@gmail.com>
2022-09-04 13:19:42 -0500

    module profile

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ use App\Http\Controllers\SpeakerController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
