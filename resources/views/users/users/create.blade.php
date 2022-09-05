ed1c795febf9c886d959a6e6810175aa530de400
cristobal nyram <crisco.nyram@gmail.com>
2022-09-05 08:32:13 -0500

    advance in select 2

--- resources/views/users/users/create.blade.php
+++ resources/views/users/users/create.blade.php
@@ -57,17 +57,32 @@
 
 
 
+                            <label for="name">Rol</label>
                             <div class="form-group">
-                                <label for="role_id">Rol</label>
+                                <div class="select" id="select">
+                                    <div class="contenido-select">
 
-                                <select class="single-select-2 " data-toggle="select2"  name="role_id" id="role_id" style="width: 100%;  margin:20px;" >
-                                    <option value="-2" selected>Seleccionar</option>
+                                        <label class="titulo">Seleccione un rol</label>
+                                    </div>
+                                    <i class="fas fa-angle-down"></i>
+                                </div>
+
+                                <div class="opciones" id="opciones">
                                     @foreach ($rol_available as $rol )
-                                    <option value="{{ $rol->id }}" >{{ $rol->name }}</option>
-
+                                    <a href="#" class="opcion">
+                                        <div class="contenido-opcion">
+                                            <img src="" alt="image_rol">
+                                            <div class="textos">
+                                                <h1 class="titulo">{{ $rol->name }}</h1>
+                                                <p class="descripcion">{{ $rol->name }}</p>
+                                            </div>
+                                        </div>
+                                    </a>
                                     @endforeach
-                                  </select>
+                                </div>
                             </div>
+
+
                             <div class="form-group">
                                 <label for="gender">Genero</label>
 
@@ -124,10 +139,161 @@
 
         @include('layouts.footers.auth')
     </div>
-@endsection
+
+    <style>
+        .contenedor {
+	width: 90%;
+	max-width: 100%;
+	margin: auto;
+	/* padding: 40px 0; */
+}
+
+
+.select {
+	background: #fff;
+
+	width: 100%;
+	box-shadow: 0px 0px 0px rgba(0, 0, 0, .16);
+	border-radius: 10px;
+	cursor: pointer;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	transition: .2s ease all;
+	/* margin-bottom: 30px; */
+	padding-top:.8rem;
+    padding-left:.9rem;
+    padding-right: .6rem;
+	position: relative;
+	z-index: 200;
+	border: 2px solid transparent;
+    border: 2px solid #CAD1D7;
+
+}
+
+.select.active,
+.select:hover {
+	box-shadow: 0px 5px 10px rgba(0, 0, 0, .16);
+	border: 2px solid #3aaa35;
+}
+
+.select.active:before {
+	content: "";
+	display: block;
+	height: 0;
+	width: 0;
+	border-top: 15px solid #3aaa35;
+	border-right: 15px solid transparent;
+	border-bottom: 15px solid transparent;
+	border-left: 15px solid transparent;
+	position: absolute;
+	bottom: -30px;
+	left: calc(50% - 15px);
+}
+
+.select i {
+	font-size: 30px;
+	margin-left: 30px;
+	color: #213f8f;
+}
+
+.titulo {
+	margin-bottom: 10px;
+	color: #B6BDC4;
+	/* font-weight: 600; */
+	font-size: inherit;
+}
+
+.descripcion {
+	font-size: 18px;
+	color: #434343;
+}
+
+.opciones {
+	background: #fff;
+	border-radius: 10px;
+	box-shadow: 0px 5px 10px rgba(0,0,0,.16);
+	max-height:auto;
+	overflow: auto;
+	z-index: 100;
+	width: 100%;
+	display: none;
+    margin-top: 1.2rem;
+}
+
+.opciones.active {
+	display: block;
+	animation: fadeIn .3s forwards;
+
+}
+
+@keyframes fadeIn {
+	from {
+		transform: translateY(-200px) scale(.5);
+	}
+	to {
+		transform: translateY(0) scale(1);
+	}
+}
+
+.contenido-opcion {
+	width: 100%;
+	display: flex;
+	align-items: center;
+	transition: .2s ease all;
+}
+
+.opciones .contenido-opcion {
+	padding: .5rem;
+}
+
+.contenido-opcion img {
+	width: 60px;
+	height: 60px;
+	margin-right: 30px;
+}
+
+.opciones .contenido-opcion:hover {
+	   color: white;
+    background-color:#1f1f1f;
+}
+
+.opciones .contenido-opcion:hover .titulo,
+.opciones .contenido-opcion:hover .descripcion {
+	color: #fff;
+}
+
+@media screen and (max-width: 800px){
+	.selectbox {
+		width: 100%;
+	}
+}
+    </style>
+    @endsection
 
 @push('js')
 
+    <script>
+        const select = document.querySelector('#select');
+const opciones = document.querySelector('#opciones');
+const contenidoSelect = document.querySelector('#select .contenido-select');
+const hiddenInput = document.querySelector('#inputSelect');
+
+document.querySelectorAll('#opciones > .opcion').forEach((opcion) => {
+	opcion.addEventListener('click', (e) => {
+		e.preventDefault();
+		contenidoSelect.innerHTML = e.currentTarget.innerHTML;
+		select.classList.toggle('active');
+		opciones.classList.toggle('active');
+		hiddenInput.value = e.currentTarget.querySelector('.titulo').innerText;
+	});
+});
+
+select.addEventListener('click', () => {
+	select.classList.toggle('active');
+	opciones.classList.toggle('active');
+});
+    </script>
     <script src="/assets/js/select2.js"></script>
 
     <script src="/assets/js/validations/generalFunctions.js"></script>
