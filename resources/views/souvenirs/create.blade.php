d5997895723fc37db19f21bdb8c4c4102ee6a8d8
GerardoRufino <95055319+GerardoRufino@users.noreply.github.com>
2022-09-05 08:05:24 -0500

    The validate image function is located in the javascript file

--- resources/views/souvenirs/create.blade.php
+++ resources/views/souvenirs/create.blade.php
@@ -71,29 +71,7 @@
                         </button>
                     </div>
 
-                    <script type="text/javascript">
-                        function LimitAttach(tField, iType) {
-                            file = tField.value;
-                            if (iType == 1) {
-                                extArray = new Array(".jpeg", ".jpe", ".jpg", ".png");
-                            }
-                            allowSubmit = false;
-                            if (!file) return;
-                            while (file.indexOf("\\") != -1) file = file.slice(file.indexOf("\\") + 1);
-                            ext = file.slice(file.indexOf(".")).toLowerCase();
-                            for (var i = 0; i < extArray.length; i++) {
-                                if (extArray[i] == ext) {
-                                    allowSubmit = true;
-                                    document.getElementById('alerta').style.display = "none";
-                                    break;
-                                }
-                            }
-                            if (allowSubmit) {} else {
-                                tField.value = "";
-                                document.getElementById('alerta').style.display = "block";
-                            }
-                        }
-                    </script>
+                   
 
 
                     <div class="row  mt-5 d-flex justify-content-center">
