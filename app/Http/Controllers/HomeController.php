8e64acc21266a275902daa6e756fcda4226258a3
ALBERTH-CMD <GomezAlbertOlivares@gmail.com>
2022-09-26 10:43:41 -0500

    upf

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Logbook;
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     /**
@@ -21,7 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $log=new Logbook();
+
+        //$role = New Role();
+
+        $log = new Logbook();
         $log->activity_done($description = 'Accedió a la página principal', $table_id = 0, $menu_id = 5, $user_id = Auth::id(), $kind_acction = 1);
         //Logbook::activity_done($description = 'Accedió a la página principal', $table_id = 0, $menu_id = 5, $user_id = Auth::id(), $kind_acction = 1);
         $variables=[
