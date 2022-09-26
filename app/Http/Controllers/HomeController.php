27e43ee8a4720da58c1120e097170d0fa7de1cdd
ALBERTH-CMD <GomezAlbertOlivares@gmail.com>
2022-09-26 10:42:19 -0500

    update changes errors resolve

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,6 +21,75 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $log=new Logbook();
+        $log->activity_done($description = 'Accedió a la página principal', $table_id = 0, $menu_id = 5, $user_id = Auth::id(), $kind_acction = 1);
+        //Logbook::activity_done($description = 'Accedió a la página principal', $table_id = 0, $menu_id = 5, $user_id = Auth::id(), $kind_acction = 1);
+        $variables=[
+            'menu'=>'dashboard',
+            'title_page'=>'dashboard',
+
+
+        ];
+        return view('dashboard')->with($variables);
+    }
+    public function home()
+    {
+        $variables=[
+            'menu'=>'dashboard',
+            'title_page'=>'dashboard',
+
+
+        ];
+        return view('home_page.index')->with($variables);
+    }
+    public function school_secretary()
+    {
+        $variables=[
+            'menu'=>'dashboard',
+            'title_page'=>'dashboard',
+
+
+        ];
+        return view('home_page.index')->with($variables);
+    }
+    public function about_us()
+    {
+        $variables=[
+            'menu'=>'dashboard',
+            'title_page'=>'dashboard',
+
+
+        ];
+        return view('home_page.index')->with($variables);
+    }
+    public function school_services()
+    {
+        $variables=[
+            'menu'=>'dashboard',
+            'title_page'=>'dashboard',
+
+
+        ];
+        return view('home_page.index')->with($variables);
+    }
+    public function school_galery()
+    {
+        $variables=[
+            'menu'=>'dashboard',
+            'title_page'=>'dashboard',
+
+
+        ];
+        return view('home_page.index')->with($variables);
+    }
+    public function school_institution()
+    {
+        $variables=[
+            'menu'=>'dashboard',
+            'title_page'=>'dashboard',
+
+
+        ];
+        return view('home_page.index')->with($variables);
     }
 }
