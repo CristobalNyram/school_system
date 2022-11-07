3d58d6b38e01a81117dcd6526a9d1ae298ec79bd
antonio12p <tobiashuerta1234@gmail.com>
2022-11-07 02:21:30 -0600

    install nucleo datatables

--- resources/views/layouts/headers/cards.blade.php
+++ resources/views/layouts/headers/cards.blade.php
@@ -5,12 +5,9 @@
     @include('dashboard.admin_info')
 
     @case(4)
-<<<<<<< HEAD
     @case(5)
     @include('dashboard.student_info')
-=======
-    {{-- @include('dashboard.student_info') --}}
->>>>>>> e34c7049456cd23cd4cb47fac4457b9c170c8b03
+
 
         @break
 
