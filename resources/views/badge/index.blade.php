fe48ff285785baae7a340308317f8923071724d9
antoniomora2406 <mmoraleshernandez617@gmail.com>
2022-11-29 13:13:47 -0600

    changes

--- resources/views/badge/index.blade.php
+++ resources/views/badge/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 @include('layouts.navbars.navs.header')
-
+<link rel="stylesheet" href="{{ asset('assets/css/home') }}/Estilo_Gafet.css">
 
 <div class="container-fluid mt--6">
     <div class="row d-flex mb-3 mr-5 justify-content-end">
